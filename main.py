9b7b14a34a18d89a6d7c112fd91a6060bc6c7de1
rodcord <rodbonillacordova@.com>
2026-09-19 14:40:02 -0600

    fix: corregida configuracion de CORS y agregados endpoints de autenticacion

--- main.py
+++ main.py
@@ -92,9 +92,16 @@ class UsuarioCreate(BaseModel):
 app = FastAPI(title="Liquor Store API - Módulo Catálogo")
 
 # Configuración de CORS única
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
