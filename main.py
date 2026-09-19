2cbcef5238a0753fdf1f0a3ef5b96a5b2ad5b19f
rodcord <rodbonillacordova@.com>
2026-09-19 14:54:44 -0600

    fix: actualizo main.py con CORS y archivos del frontend

--- main.py
+++ main.py
@@ -97,6 +97,8 @@ origins = [
     "http://localhost:5500",
     "http://127.0.0.1:8000",
     "http://localhost:8000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
